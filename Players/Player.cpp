136afa16f87d242f74880cb727c0546d53f60287
Daniel Sitbon <sitbondaniel@campus.technion.ac.il>
2023-06-13 11:15:10 +0300

    Fichiers crées

---
+++ Players/Player.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include "Player.h"
+
+
+Player::Player(const char* name, int maxHp, int force)
+{
+	m_name = name;
+	m_level = START_LEVEL;
+	m_coins = START_COINS;
+	if (maxHp <= 0)
+	{
+		m_maxHp = DEFAULT_MAX_HP;
+		m_healthPoints = DEFAULT_MAX_HP;
+	}
+	else
+	{
+		m_maxHp = maxHp;
+		m_healthPoints = maxHp;
+	}
+	if (force <= 0)
+	{
+		m_force = DEFAULT_FORCE;
+	}
+	else
+	{
+		m_force = force;
+	}
+}
+
+void Player::printInfo() const
+{
+	printPlayerInfo(m_name.c_str(), m_level, m_force, m_healthPoints, m_coins);
+}
+
+void Player::levelUp()
+{
+	if (m_level < MAX_LEVEL)
+	{
+		m_level++;
+	}
+}
+
+int Player::getLevel() const
+{
+	return m_level;
+}
+
+void Player::buff(const int force)
+{
+	if (force > 0)
+	{
+		m_force += force;
+	}
+}
+
+void Player::heal(const int hp)
+{
+	if (hp > 0)
+	{
+		m_healthPoints += hp;
+		if (m_healthPoints > m_maxHp)
+		{
+			m_healthPoints = m_maxHp;
+		}
+	}
+}
+
+void Player::damage(const int hp)
+{
+	if (hp > 0)
+	{
+		m_healthPoints -= hp;
+		if (m_healthPoints < 0)
+		{
+			m_healthPoints = 0;
+		}
+	}
+}
+
+bool Player::isKnockedOut() const
+{
+	return m_healthPoints <= 0;
+}
+
+void Player::addCoins(const int coins)
+{
+	if (coins > 0)
+	{
+		m_coins += coins;
+	}
+}
+
+bool Player::pay(const int coins)
+{
+	if (coins < 0)
+	{
+		return true;
+	}
+	if (m_coins - coins < 0)
+	{
+		return false;
+	}
+	m_coins -= coins;
+	return true;
+}
+
+int Player::getAttackStrength() const
+{
+	return m_force + m_level;
+}
---
+++ Players/Player.h
@@ -0,0 +1,117 @@
+#ifndef EX2_PLAYER_H
+#define EX2_PLAYER_H
+
+#include "../utilities.h"
+
+
+const int DEFAULT_FORCE = 5;
+const int DEFAULT_MAX_HP = 100;
+const int START_LEVEL = 1;
+const int START_COINS = 0;
+const int MAX_LEVEL = 10;
+
+class Player
+{
+
+public:
+	/*
+	* Constructor of Player class
+	* @param name - The name of the player.
+	* @param maxHp - The maximum health points of the player.
+	* @param force - The force of the player.
+	* @return
+	*	     A new instance of Player.
+	*/
+	Player(const char* name, int maxHp = DEFAULT_MAX_HP, int force = DEFAULT_FORCE);
+
+	/*
+	* Copy Constructor of Player class
+	* @param other - The player to copy.
+	* @return
+	*      A new instance of Player.
+	*/
+	Player(const Player& other) = default;
+
+	/*
+	* Destructor of Player class
+	*/
+	~Player() = default;
+
+	/*
+	* Assignment Operator
+	* @param other - The player to copy.
+	* @return
+	*      A reference to this instance of Player.
+	*/
+	Player& operator=(const Player& other) = default;
+
+	/*
+	* Prints the player's info
+	*/
+	void printInfo() const;
+
+	/*
+	* Level up the player
+	*/
+	void levelUp();
+
+	/*
+	* Get the level of the player
+	* @return
+	*      The level of the player.
+	*/
+	int getLevel() const;
+
+	/*
+	* Calculates the attack strength of the player
+	* @return
+	*      The attack strength of the player.
+	*/
+	int getAttackStrength() const;
+
+	/*
+	* Buffs the player's force by 'force' points
+	* @param force - The number of points to buff.
+	*/
+	void buff(const int force);
+
+	/*
+	* Heals the player's health points by 'hp' points
+	* @param hp - The number of points to heal.
+	*/
+	void heal(const int hp);
+
+	/*
+	* Damages the player's health points by 'hp' points
+	* @param hp - The number of points to damage.
+	*/
+	void damage(const int hp);
+
+	/*
+	* Checks if the player is knocked out
+	* @return true if the player is knocked out, false otherwise.
+	*/
+	bool isKnockedOut() const;
+
+	/*
+	* Add coins to the player
+	* @param coins - The number of coins to add.
+	*/
+	void addCoins(const int coins);
+
+	/*
+	* Pay coins from the player
+	* @param coins - The number of coins to pay.
+	* @return true if the player has enough coins, false otherwise.
+	*/
+	bool pay(const int coins);
+
+private:
+	std::string m_name;
+	int m_maxHp;
+	int m_healthPoints;
+	int m_force;
+	int m_level;
+	int m_coins;
+};
+#endif
